731dbc7718d15729cc3acab104960d654bc5ce38
Bibelsammler <bibelsammler@uni.de>
2015-08-28 23:31:58 +0200

    first commit

---
+++ metadata.js
@@ -0,0 +1,218 @@
+biblename = "Münchener Neues Testament (1998) [www.obohu.cz]";
+metadata = [{
+abbr:"Mt",
+short:"Matthäus",
+long:"Matthäus",
+osis:"Matt",
+type:"nt",
+nt:true,
+chapters:28
+},{
+abbr:"Markus",
+short:"Markus",
+long:"Markus",
+osis:"Mark",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"Lk",
+short:"Lukas",
+long:"Lukas",
+osis:"Luke",
+type:"nt",
+nt:true,
+chapters:24
+},{
+abbr:"Joh",
+short:"Johannes",
+long:"Johannes",
+osis:"John",
+type:"nt",
+nt:true,
+chapters:21
+},{
+abbr:"Apg",
+short:"Apostelgeschichte",
+long:"Apostelgeschichte",
+osis:"Acts",
+type:"nt",
+nt:true,
+chapters:28
+},{
+abbr:"Röm",
+short:"Römer",
+long:"Römer",
+osis:"Rom",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"1.Kor",
+short:"1. Korinther",
+long:"1. Korinther",
+osis:"1Cor",
+type:"nt",
+nt:true,
+chapters:16
+},{
+abbr:"2.Kor",
+short:"2. Korinther",
+long:"2. Korinther",
+osis:"2Cor",
+type:"nt",
+nt:true,
+chapters:13
+},{
+abbr:"Gal",
+short:"Galater",
+long:"Galater",
+osis:"Gal",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"Eph",
+short:"Epheser",
+long:"Epheser",
+osis:"Eph",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"Phil",
+short:"Philipper",
+long:"Philipper",
+osis:"Phil",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"Kol",
+short:"Kolosser",
+long:"Kolosser",
+osis:"Col",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"1.Thes",
+short:"1. Thessalonicher",
+long:"1. Thessalonicher",
+osis:"1Thess",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2.Thes",
+short:"2. Thessalonicher",
+long:"2. Thessalonicher",
+osis:"2Thess",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"1.Tim",
+short:"1. Timotheus",
+long:"1. Timotheus",
+osis:"1Tim",
+type:"nt",
+nt:true,
+chapters:6
+},{
+abbr:"2.Tim",
+short:"2. Timotheus",
+long:"2. Timotheus",
+osis:"2Tim",
+type:"nt",
+nt:true,
+chapters:4
+},{
+abbr:"Tit",
+short:"Titus",
+long:"Titus",
+osis:"Titus",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"Phlm",
+short:"Philemon",
+long:"Philemon",
+osis:"Phlm",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Heb",
+short:"Hebräer",
+long:"Hebräer",
+osis:"Heb",
+type:"nt",
+nt:true,
+chapters:13
+},{
+abbr:"Jak",
+short:"Jakobus",
+long:"Jakobus",
+osis:"Jas",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"1.Pet",
+short:"1. Petrus",
+long:"1. Petrus",
+osis:"1Pet",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2.Pet",
+short:"2. Petrus",
+long:"2. Petrus",
+osis:"2Pet",
+type:"nt",
+nt:true,
+chapters:3
+},{
+abbr:"1.Joh",
+short:"1. Johannes",
+long:"1. Johannes",
+osis:"1John",
+type:"nt",
+nt:true,
+chapters:5
+},{
+abbr:"2.Joh",
+short:"2. Johannes",
+long:"2. Johannes",
+osis:"2John",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"3.Joh",
+short:"3. Johannes",
+long:"3. Johannes",
+osis:"3John",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Jud",
+short:"Judas",
+long:"Judas",
+osis:"Jude",
+type:"nt",
+nt:true,
+chapters:1
+},{
+abbr:"Off",
+short:"Offenbarung",
+long:"Offenbarung",
+osis:"Rev",
+type:"nt",
+nt:true,
+chapters:22
+}];
